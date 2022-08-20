e60308d4f3cc153782b03d87deb58dc5139d214c
ishtaar <ishtaar.desravines@gmail.com>
2022-08-20 14:06:36 -0400

    updated MEM and CPU file

--- autoMemcpu.sh
+++ autoMemcpu.sh
@@ -3,10 +3,15 @@
 # Objective: 
 # Script obtains the current memory and cpu usage of processes on the system and outputs it to a file. Then sends that file to the remote repository every minute. 
 
-#CronJob 
+#Crontab for every minute: * * * * *
 
 
 cpuMem=$(ps -eo pid,%cpu,%mem,comm)
 
-echo "$cpuMem" > ~/Desktop/sysMemcpu.txt
+echo "$cpuMem" > ~/Diagnostic-5/sysMemcpu.txt
 
+git add .
+
+git commit -m "updated MEM and CPU file"
+
+git push origin main 
